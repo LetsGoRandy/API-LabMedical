df6622d5b62ae0492b919bda47995470a9ff4ec5
Randy Gomes <raandy.gc@gmail.com>
2024-10-06 01:57:43 -0300

    Implementação dos Endpoints de Usuarios

--- src/main/java/com/labmedicine/api_labmedication/controllers/UsuarioController.java
+++ src/main/java/com/labmedicine/api_labmedication/controllers/UsuarioController.java
@@ -3,8 +3,13 @@ package com.labmedicine.api_labmedication.controllers;
 import com.labmedicine.api_labmedication.dtos.UsuarioDTO;
 import com.labmedicine.api_labmedication.services.UsuarioService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -17,10 +22,40 @@ public class UsuarioController {
     }
 
     @PostMapping
-    public ResponseEntity<UsuarioDTO> criarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO) {
-        UsuarioDTO usuarioCriado = usuarioService.criarUsuario(usuarioDTO);
-        return ResponseEntity.status(201).body(usuarioCriado);
-
+    public ResponseEntity<UsuarioDTO> novoUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO){
+        usuarioDTO = usuarioService.criarUsuario(usuarioDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(usuarioDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(usuarioDTO);
     }
 
+    @GetMapping
+    public ResponseEntity<Page<UsuarioDTO>> listarUsuarios(Pageable pageable) {
+        Page<UsuarioDTO> usuariosDTO = usuarioService.listarUsuarios(pageable);
+        return ResponseEntity.ok(usuariosDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> usuarioById(@PathVariable Long id) {
+        UsuarioDTO usuarioDTO = usuarioService.buscarById(id);
+        return ResponseEntity.ok(usuarioDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> atualizarUsuario(@PathVariable Long id,@Valid @RequestBody UsuarioDTO usuarioDTO) {
+        usuarioDTO = usuarioService.atualizarUsuario(id, usuarioDTO);
+        return ResponseEntity.ok(usuarioDTO);
+    }
+
+    @PutMapping("/{id}/senha")
+    public ResponseEntity<UsuarioDTO> atualizarSenha(@PathVariable Long id,@Valid @RequestBody UsuarioDTO usuarioDTO) {
+        usuarioDTO = usuarioService.atualizarSenha(id, usuarioDTO);
+        return ResponseEntity.ok(usuarioDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+        usuarioService.deletarUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/labmedicine/api_labmedication/dtos/UsuarioDTO.java
+++ src/main/java/com/labmedicine/api_labmedication/dtos/UsuarioDTO.java
@@ -1,16 +1,22 @@
 package com.labmedicine.api_labmedication.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.labmedicine.api_labmedication.models.EspecializacaoClinica;
 import com.labmedicine.api_labmedication.models.EstadoCivil;
-import com.labmedicine.api_labmedication.models.Pessoa;
 import com.labmedicine.api_labmedication.models.Usuario;
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
+
+    private Long id;
 
     @NotBlank(message = "O nome completo é obrigatório")
     private String nomeCompleto;
@@ -20,35 +26,54 @@ public class UsuarioDTO {
 
     @NotNull(message = "A data de nascimento é obrigatória")
     @Past(message = "A data de nascimento deve estar no passado")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataNascimento;
 
     @NotBlank(message = "O CPF é obrigatório")
-    @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", message = "O CPF deve estar no formato 000.000.000-00")
     private String cpf;
 
     @NotBlank(message = "O RG é obrigatório")
+    @Size(max = 20, message = "O RG deve ter no máximo 20 caracteres")
     private String rg;
 
     @NotBlank(message = "O telefone é obrigatório")
+    @Pattern(regexp = "\\(\\d{2}\\) \\d{5}-\\d{4}", message = "O telefone deve estar no formato (99) 99999-9999")
     private String telefone;
 
-    @Email(message = "O email é obrigatório")
+    @Email(message = "O email deve ser válido")
     private String email;
 
     @NotBlank(message = "A naturalidade é obrigatória")
+    @Size(min = 8, max = 64, message = "A naturalidade deve ter entre 8 e 64 caracteres")
     private String naturalidade;
 
-    @NotBlank(message = "O crm é obrigatório")
-    private String crm;
-
-    @NotBlank(message = "O estado civil é obrigatório")
+    @NotNull(message = "O estado civil é obrigatório")
     private EstadoCivil estadoCivil;
 
-    @NotBlank(message = "A especialização clínica é obrigatória")
+    @NotBlank(message = "O CRM é obrigatório")
+    private String crm;
+
+    @NotNull(message = "A especialização clínica é obrigatória")
     private EspecializacaoClinica especializacaoClinica;
 
-    @NotBlank(message = "A senha é obrigatória")
-    @Size(min = 8, message = "A senha deve conter no mínimo 8 dígitos")
+    @Size(min = 8, message = "A Senha deve ter pelo menos 8 caracteres")
     private String senha;
+
+    public UsuarioDTO(Usuario usuario) {
+
+        id = usuario.getId();
+        nomeCompleto = usuario.getNomeCompleto();
+        genero = usuario.getGenero();
+        dataNascimento = usuario.getDataNascimento();
+        cpf = usuario.getCpf();
+        rg = usuario.getRg();
+        telefone = usuario.getTelefone();
+        email = usuario.getEmail();
+        naturalidade = usuario.getNaturalidade();
+        estadoCivil = usuario.getEstadoCivil();
+        crm = usuario.getCrm();
+        especializacaoClinica = usuario.getEspecializacaoClinica();
+        senha = usuario.getSenha();
+    }
 
 }
